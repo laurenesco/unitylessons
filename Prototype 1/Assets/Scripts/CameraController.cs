461af3c8b28ec6fb9bd50993f35064eeb1a3ef04
laurenesco <89123282+laurenesco@users.noreply.github.com>
2025-12-17 20:06:26 -0600

    Initial project commit

---
+++ Prototype 1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Variables
+    public GameObject player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 5, -7);   
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Variables
+    public float speed = 5.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
