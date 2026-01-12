10cccdc1eab92c22abf87d4b442865600f68f9f0
laurenesco <89123282+laurenesco@users.noreply.github.com>
2026-01-11 18:09:38 -0600

    2.3 complete

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spawn animals
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int index = Random.Range(0, animalPrefabs.Length);
+            int spawn_location = Random.Range(-18, 20);
+            Instantiate(animalPrefabs[index], new Vector3(spawn_location, 0, 20), animalPrefabs[index].transform.rotation);
+        }
+    }
+}
